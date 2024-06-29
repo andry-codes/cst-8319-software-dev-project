87740e1f36551a88d3d05b40b57d6b1003fd54c0
Alex French <alexander.l.french@gmail.com>
2024-06-29 18:59:59 -0400

    early servlets and dao work (meant to go with last push).

---
+++ src/main/java/servlets/RegisterServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Register;
+import dao.RegistrationDao;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	List<Register> user = new ArrayList<>();
+
+    	/* CRUD operations for creating new user. 
+    	 * 
+    	 */
+
+   }
+}
